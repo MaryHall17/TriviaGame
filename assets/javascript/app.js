6c51b7ed5d279edb4f7051a3d40b6bcfadba265d
Mary Hall <mh6uj@students-MBP.home>
2017-11-20 20:50:57 -0500

    timer, need to fix interval bug

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -15,10 +15,6 @@
 		//show flunked screen
 	//}
 
-//Timer function
-
-
-
 $(document).ready(function assignValues() {
 	//Variable assignment:
 	var ans1=1; //assigned value to correct answer for Q1
@@ -33,6 +29,25 @@ $(document).ready(function assignValues() {
 	console.log(ans2);
 	console.log(ans3);
 	console.log(ans4);
+	//Timer function
+	var number = 60 //set our number to 60 for a minute
+	 //this variable will hold the interval for the countdown function
+	
+	function countdown() {
+		setInterval(decrement, 1000); //decreases number by one every second
+	};
+	console.log(countdown());
+	//Function to decrement the timer
+	function decrement() {
+		number--; //decreases the number variable by one 
+	};
+
+	$("#timer").html("<h2>" + number + "<h2>"); //shows the timer as the number variable in the top div
 
 });
+
+
+
+
+
 
