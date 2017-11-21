6bbf48aed1fba4fc9f9afd9a0a08a1c5bb164cdc
Mary Hall <mh6uj@students-MBP.home>
2017-11-21 15:59:04 -0500

    got scoring function to run

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -14,23 +14,89 @@
 	//} else {
 		//show flunked screen
 	//}
+	// function score(optradio) {
+	//  var point1 =parseInt(document.getElementById("ans1").value);
+	//  var point2 =parseInt(document.getElementById("ans2").value);
+	//  var point3 =parseInt(document.getElementById("ans3").value);
+	//  var point4 =parseInt(document.getElementById("ans4").value);
+	//  var score = point1 + point2 + point3 + point4;
 
-$(document).ready(function assignValues() {
-	//Variable assignment:
-	var ans1=1; //assigned value to correct answer for Q1
-	var ans2=2; //assigned value to correct answer for Q2
-	var ans3=3;	//assigned value to correct answer for Q3
-	var ans4=4; //assigned value to correct answer for Q4
-	$("#ans1").html(ans1);
-	$("#ans2").html(ans2);
-	$("#ans3").html(ans3);
-	$("#ans4").html(ans4);
-	console.log(ans1);
-	console.log(ans2);
-	console.log(ans3);
-	console.log(ans4);
+	//  console.log(score);
+	//  console.log(point1);
+	//  console.log(point2);
+	//  console.log(point3);
+	//  console.log(point4);
+	// };
+	//Collect values from answers to pass into "if checked" statement
+	// var ans1 = $("#ans1").checked
+	// var ans2 = $("#ans2").checked
+	// var ans3 = $("#ans3").checked
+	// var ans4 = $("#ans4").checked
+
+	//Parse values from answers into integers to later use for score.
+	// var point1 = parseInt(document.getElementById("ans1").value); 
+	var point2 = parseInt(document.getElementById("ans2").value);
+	var point3 = parseInt(document.getElementById("ans3").value);
+	var point4 = parseInt(document.getElementById("ans4").value);
+
+	
+	var integer = 0;//variable that holds our score
+	baseScore = parseInt(integer);
+
+		// function score() { 
+		// 		if ($("#ans1".is(":checked")) { 
+		// 			alert("You got one right!");
+		// 			baseScore++;
+		// 		} 
+		// 		if (ans2 = true) {
+		// 			alert("You got two right");
+		// 			baseScore++;
+		// 		} 
+		// 		if (ans3 = true) {
+		// 			alert("You got three right");
+		// 			baseScore++;
+		// 		} 
+		// 		if (ans4 = true) {
+		// 			alert("You got four right")
+		// 			baseScore++;
+		// 		}
+		// 	} 
+
+
+$(document).ready(function triviaGame() {
+
+	//Find if the answers are clicked, if it they are clicked, add to the score
+	//First answer
+	$("#ans1").click(function answer1() {
+		if($("#ans1").is(':checked')) {
+			alert("It's checked!");
+			baseScore++;
+		}
+	});
+	//Second answer
+	$("#ans2").click(function answer2() {
+		if($("#ans2").is(':checked')) {
+			alert("It's checked!")
+			baseScore++;
+		}
+	});
+	//Third answer
+	$("#ans3").click(function answer3() {
+		if($("#ans3").is(':checked')) {
+			alert("It's checked!")
+			baseScore++;
+		}
+	});
+	// Fourth answer
+	$("#ans4").click(function answer4() {
+		if($("#ans4").is(':checked')) {
+			alert("It's checked!")
+			baseScore++;
+		}
+	});
+
 	//Timer function
-	var number = 60 //set our number to 60 for a minute
+	var number = 25 //set our number to 60 for a minute
 	var intervalId; //this variable will hold the interval for the countdown function
 	
 	function countdown() {
@@ -48,11 +114,13 @@ $(document).ready(function assignValues() {
 
 			//...run the stop function defined below
 			stop();
-			//...run the scoring function
+			//call the baseScore... 
+			console.log(baseScore);
 			// Alert time is up
 			alert("Time Up!");
 		}
-	};
+	}; 
+
 
 	//The stop function to clear the intervalId
 	function stop() {
