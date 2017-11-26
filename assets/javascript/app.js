d0c044c84894de9eb7e1f9349ed62fde86ef8d21
Mary Hall <mh6uj@students-MBP.home>
2017-11-26 10:20:25 -0500

    edited to allow for toggling

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,112 +1,8 @@
-// Psuedo Code:
-	//Timer that counts down from one minute after play button is clicked
-		//Use time converter and decrementer from stopwatch exercise
-	//Assign values to buttons
-	//Match values to variable
-	//Nest score count in time run out function, so score doesn't add for every click
-	//if (answers array value=button value) {
-		//add one to invisible score
-		//}
-	//if (score=4) {
-		//show aced screen
-	//}else if (score=3) {
-		//show mediocre screen
-	//} else {
-		//show flunked screen
-	//}
-	// function score(optradio) {
-	//  var point1 =parseInt(document.getElementById("ans1").value);
-	//  var point2 =parseInt(document.getElementById("ans2").value);
-	//  var point3 =parseInt(document.getElementById("ans3").value);
-	//  var point4 =parseInt(document.getElementById("ans4").value);
-	//  var score = point1 + point2 + point3 + point4;
-
-	//  console.log(score);
-	//  console.log(point1);
-	//  console.log(point2);
-	//  console.log(point3);
-	//  console.log(point4);
-	// };
-	//Collect values from answers to pass into "if checked" statement
-	// var ans1 = $("#ans1").checked
-	// var ans2 = $("#ans2").checked
-	// var ans3 = $("#ans3").checked
-	// var ans4 = $("#ans4").checked
-
-	//Parse values from answers into integers to later use for score.
-	// var point1 = parseInt(document.getElementById("ans1").value); 
-	var point2 = parseInt(document.getElementById("ans2").value);
-	var point3 = parseInt(document.getElementById("ans3").value);
-	var point4 = parseInt(document.getElementById("ans4").value);
-
-	
-	var integer = 0;//variable that holds our score
-	baseScore = parseInt(integer);
-
-		// function score() { 
-		// 		if ($("#ans1".is(":checked")) { 
-		// 			alert("You got one right!");
-		// 			baseScore++;
-		// 		} 
-		// 		if (ans2 = true) {
-		// 			alert("You got two right");
-		// 			baseScore++;
-		// 		} 
-		// 		if (ans3 = true) {
-		// 			alert("You got three right");
-		// 			baseScore++;
-		// 		} 
-		// 		if (ans4 = true) {
-		// 			alert("You got four right")
-		// 			baseScore++;
-		// 		}
-		// 	} 
+var integer = 0;//variable that holds our score
+baseScore = parseInt(integer); //parse as integer to add up score
 
 
 $(document).ready(function triviaGame() {
-
-	//Find if the answers are clicked, if it they are clicked, add to the score
-	//Run the functions once because of toggling
-	//First answer
-	$("#ans1").one("click",function answer1() { //run the function only once for "toggling"
-		if($("#ans1").is(':checked')) {
-			baseScore++;
-		} else if ($("#ans1").is(':unchecked')) { //subtract from score if toggled from correct to incorrect answer
-			baseScore--;
-		} else {
-			baseScore++; //add back to score if toggled from correct to incorrect to correct
-		}
-	});
-	//Second answer
-	$("#ans2").one("click",function answer2() { //run the function only once for "toggling" 
-		if($("#ans2").is(':checked')) {
-			baseScore++;
-		} else if ($("#ans2").is('unchekced')) { //subtract from score if toggled from correct to incorrect
-			baseScore--;
-		} else {
-			baseScore++; //add back to score if toggled from correct to incorrect to correct
-		}
-	});
-	//Third answer
-	$("#ans3").click("click",function answer3() { //run the function only once for "toggling"
-		if($("#ans3").is(':checked')) {
-			baseScore++;
-		} else if ($("#ans3").is('unchekced')) { //subtract from score if toggled from correct to incorrect
-			baseScore--;
-		} else {
-			baseScore++; //add back to score if toggled from correct to incorrect to correct
-		}
-	});
-	// Fourth answer
-	$("#ans4").click("click",function answer4() { //run the function only once for "toggling"
-		if($("#ans4").is(':checked')) {
-			baseScore++;
-		} else if ($("#ans4").is('unchekced')) { //subtract from score if toggled from correct to incorrect
-			baseScore--;
-		} else {
-			baseScore++; //add back to score if toggled from correct to incorrect to correct
-		}
-	});
 
 	//Timer function
 
@@ -128,17 +24,44 @@ $(document).ready(function triviaGame() {
 
 			//...run the stop function defined below
 			stop();
+			
+			//Scoring if statements...
+			//Query whether it the radio button is checked or not after time is up to allow for toggling.
+			if($("#ans1").is(':checked')) {
+				baseScore++;
+			};
+			if($("#ans2").is(':checked')){
+				baseScore++;
+			};
+			if($("#ans3").is(':checked')) {
+				baseScore++;
+			};
+			if($("#ans4").is(':checked')) {
+				baseScore++;
+			};
+			
 			//call the baseScore in the console to run scoring if/else logic...
 			console.log(baseScore);
+			
 			// Alert time is up
 			alert("Time Up!"); 
-			//Scoring if/else logic
+			
+			//Scoring if/else logic for css changes
 			if(baseScore==4) {
-				alert("Score is " + baseScore + " " + "out of 4 " + "You aced it!");
+				//Change from original background to aced screen:
+				$("body").toggleClass("aced");
+				//Replace questions with victory message:
+				$(".container").html("Score is " + baseScore + " " + "out of 4 " + "You aced it! The foaming mouth guy is your biggest fan!");
 			} else if(baseScore==3) {
-				alert("Score is " + baseScore + " " + "out of 4 " +  "You're almost there!");
+				//Change from original background to mediocre screen:
+				$("body").toggleClass("mediocre");
+				//Replace questions with almost there message:
+				$(".container").html("Score is " + baseScore + " " + "out of 4 " +  "You're almost there! Drink some tea with Iroh and brush up your Avatar knowledge.");
 			} else {
-				alert("Score is " + baseScore + " " + "out of 4 " +  "Season One Zuko has more honor than you!")
+				//Change from original background to failing screen:
+				$("body").toggleClass("failing");
+				//Replace questions with failing message:
+				$(".container").html("Score is " + baseScore + " " + "out of 4 " +  "Season One Zuko has more honor than you!");	
 			}
 		}
 	}; 
